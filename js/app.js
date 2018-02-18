54ff02838607af1b731576430a537b5e2b016e92
“CharlotteMD” <charlottemdavies@hotmail.co.uk>
2018-02-18 10:27:50 +0000

    added info from cv

--- js/app.js
+++ js/app.js
@@ -1,2 +1,32 @@
 console.log('working');
 console.log('ready');
+
+
+$(function (){
+
+  $('#about').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#about-section').offset().top
+    }, 2000);
+  });
+
+
+  $('#projects').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#projects-section').offset().top
+    }, 2000);
+  });
+
+  $('#cv').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#cv-section').offset().top
+    }, 2000);
+  });
+
+  $('#contact').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#contact-section').offset().top
+    }, 2000);
+  });
+
+});
