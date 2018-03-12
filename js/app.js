7a3fba61aecc679a116a045bca90fa036013e810
“CharlotteMD” <charlottemdavies@hotmail.co.uk>
2018-03-12 18:12:15 +0000

    full page scrolling working

--- js/app.js
+++ js/app.js
@@ -1,41 +1,6 @@
 console.log('working');
 console.log('ready');
 
-
-function init(){
-
-  $(document).ready(function() {
-
-    $('#fullpage').fullpage();
-  });
-
-}
-
-init();
-//
-//   $('#about').click(function() {
-//     $('html, body').animate({
-//       scrollTop: $('#about-section').offset().top
-//     }, 2000);
-//   });
-//
-//
-//   $('#projects').click(function() {
-//     $('html, body').animate({
-//       scrollTop: $('#projects-section').offset().top
-//     }, 2000);
-//   });
-//
-//   $('#cv').click(function() {
-//     $('html, body').animate({
-//       scrollTop: $('#cv-section').offset().top
-//     }, 2000);
-//   });
-//
-//   $('#contact').click(function() {
-//     $('html, body').animate({
-//       scrollTop: $('#contact-section').offset().top
-//     }, 2000);
-//   });
-//
-// });
+$(document).ready(function() {
+  $('#fullpage').fullpage();
+});
